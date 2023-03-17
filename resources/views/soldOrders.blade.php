3840c1ce7bda71fcb0919c5eb4422ce91f85edf5
Yash Sunil Ambekar <109550783+yashambekar2000@users.noreply.github.com>
2023-03-17 11:19:23 +0530

    Order managrement structure created but remaining lots of part.

--- resources/views/soldOrders.blade.php
+++ resources/views/soldOrders.blade.php
@@ -4,33 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/orderManagement.css">
-    
     <title>Document</title>
+    <link rel="stylesheet" href="css/soldOrders.css">
 </head>
 <body>
+@include('layouts/orderManagementNav') 
+<div class="ordermainclass">
 
-
- 
-  
+   
+    
+   
     <!-- <div class="searchfield">
     <input type="text" placeholder="Search here" class="searchinput">
     <button class="searchbtn">Search</button>
     </div> -->
 
-    @include('layouts/orderManagementNav') 
- <div class="ordermainclass">
+   
  
-    <table >
+<table >
         <tr >
-            <th colspan="5" class="tableheading">Approved Orders</th>
+            <th colspan="5" class="tableheading">Pending Orders</th>
         </tr>
         <tr>
             <th>Order Id</th>
             <th>Order Date</th>
             <th>Retailer</th>
             <th>Products</th>
-            <th>Status </th>
+            <th>Reason for pending</th>
         </tr>
         <tr>
             <td>1</td>
@@ -41,8 +41,6 @@
             <option value="Move to Sale">Move to Sale</option>
             <option value="Payment not Done">Payment not Done</option>
             <option value="Returned Order">Returned Order</option>
-            <option value="Sold Order">Sold Order</option>
-            <option value="Canceled Order">Canceled Order</option>
             </select></td>
         </tr>
         <tr>
@@ -54,8 +52,6 @@
             <option value="Move to Sale">Move to Sale</option>
             <option value="Payment not Done">Payment not Done</option>
             <option value="Returned Order">Returned Order</option>
-            <option value="Sold Order">Sold Order</option>
-            <option value="Canceled Order">Canceled Order</option>
             </select></td>
         </tr>
         <tr>
@@ -67,12 +63,10 @@
             <option value="Move to Sale">Move to Sale</option>
             <option value="Payment not Done">Payment not Done</option>
             <option value="Returned Order">Returned Order</option>
-            <option value="Sold Order">Sold Order</option>
-            <option value="Canceled Order">Canceled Order</option>
             </select></td>
         </tr>
     </table>
- </div>
- 
+
+    </div>   
 </body>
 </html>
